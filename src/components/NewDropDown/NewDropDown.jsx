9e005c3988eef7ff022d1b0ef8b247e02396ab98
Linh Nguyen <linh.nguyen2@wsu.edu>
2021-01-12 15:13:50 +0700

    add initial option for the list

--- src/components/NewDropDown/NewDropDown.jsx
+++ src/components/NewDropDown/NewDropDown.jsx
@@ -5,6 +5,8 @@ import DropDownItem from '../DropDownItem/DropDownItem.jsx';
 export default function NewDropDown(props){
     const listOfYears = [];
 
+    listOfYears.push(<option value="none" selected disabled hidden>Select an option</option>)
+
     for(let i = 1900; i <= 2030; i++)
     {
         listOfYears.push(<DropDownItem key={i} year={i}/>);
