63bdcfe73b1f029176f035dc56a4a58578528956
Linh Nguyen <linh.nguyen2@wsu.edu>
2021-01-22 14:20:51 +0700

    mock data for the users page

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const UsersData = [
+    {
+        name: "Linh Nguyen",
+        age:"26",
+        birth: "12/22/1995",
+        address: "804 144th PL SE, Bellevue, WA 98007"
+    },
+    {
+        name: "Nga Nguyen",
+        age:"32",
+        birth: "06/25/1989",
+        address: "804 144th PL SE, Bellevue, WA 98007"
+    },
+    {
+        name: "Hung Nguyen",
+        age:"62",
+        birth: "05/04/1959",
+        address: "11406 NE 106th Lane, Kirkland, WA, 98033"
+    },
+    {
+        name: "Thu Nguyen",
+        age:"63",
+        birth: "03/16/1958",
+        address: "5b Nguyen Binh Khiem Street, phuong Nguyen Du, Quan Hai Ba Trung, Hanoi"
+    },
+    {
+        name: "Huong Nguyen",
+        age:"38",
+        birth: "03/05/1956",
+        address: "5b Nguyen Binh Khiem Street, phuong Nguyen Du, Quan Hai Ba Trung, Hanoi"
+    }
+];
+
+export default UsersData;
